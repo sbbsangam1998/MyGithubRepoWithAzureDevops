d75cdeafd46b03886a875838fafa3b121575840e
sangambhondele9898 <sangambhondele9898@gmail.com>
2022-10-09 21:40:59 +0530

    Added Some data

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    //Added some data
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
